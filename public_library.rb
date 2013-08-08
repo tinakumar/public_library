e91cb8c67cb4d691e6cd9f2ace873fc0f795eca0
Tina Kumar <tinakumar07@gmail.com>
2013-08-08 16:24:56 -0700

    Changed to Ruby

--- public_library.rb
+++ public_library.rb
@@ -1,41 +1,79 @@
-class Book < ActiveRecord::Base
-  attr_accessible :title, :author, :available, :shelf_id
+class Book
+  #methods shelf control where book is sitting
+  attr_accessor :title, :author, :location, :status
 
-  belongs_to :shelf
-  belongs_to :library
-
-  def enshelf
-    #gives book's present location
-    location = self.shelf_id
+  def self.create_for(title, author, location, status)
+    @books ||= []
+    @books << Book.new(title, author, location, status)
   end
 
-  def unshelf
+  def initialize(title, author, location, status)
+    @title = title
+    @author = author
+    @status = status
+    @location = location
+  end
+
+  def self.find_for(location)
+    # enshelf method - defines book's present location
+    @shelf = @books.find{ |book| book.location == "#{location}"}
+  end
+
+  def unshelf(title, author)
     #maybe staging area, if book is not in proper location
-    if self.title.start_with?("A", "B", "C", "D", "E", "F", "G")
-      location == "ST3"
-    else
-      location == "ST9"
+    while @status == Available && @location == Nil
+      if self.title.start_with?("A", "B", "C", "D", "E", "F", "G")
+        location == "ST3"
+        "#{book.title} by #{book.author} is located on shelf ST3."
+      else
+        location == "ST9"
+        "#{book.title} by #{book.author} is located on shelf ST9."
+      end
     end
   end
+
 end
 
-class Library < ActiveRecord::Base
-
-  has_many :shelves
-  has_many :books
+class Library < Book
+  #should be aware of number of shelves
+  #method to report all books it contains
+  def initialize
+    @library = []
+  end
 
   def book_inventory
     @books = Book.all
   end
+  puts "There are #{books.count} books in this library"
 
   def number_of_shelves
-    @shelf = Shelf.all.count
+    if Shelf.number.unique?
+      @shelf = Shelf.number.count
+    end
   end
 end
 
-class Shelf < ActiveRecord::Base
+class Shelf < Book
+  #should know what book it contains
+  attr_accessor :number
 
-  has many :books
-  belongs_to :library
-
+  def book_on_shelf
+    @books = Book.all
+    shelf = @books.find{ |book| book.location == "#{location}"}
+    "This shelf contains #{book.title} by #{book.author}"
+  end
 end
+
+#___________________________Testing Area______________________________________
+
+#ruby public_library.rb
+#Book.create_for("The Great Gatsby", "F. Scott Fitzgerald", "G1", "Available")
+#Book.create_for("The Sun Also Rises", "Ernest Hemingway", "C9", "Available")
+#Book.create_for("Othello", "William Shakespeare", "Nil", "Available")
+#puts Book.find_for("G1")
+#"The Great Gatsby"
+#unshelf("Othello", "William Shakespeare")
+#"Othello by William Shakespeare is located on shelf ST9."
+#puts book_inventory
+#("The Great Gatsby", "F. Scott Fitzgerald", "G1", "Available"),("The Sun Also Rises", "Ernest Hemingway", "C9", "Available"), ("Othello", "William Shakespeare", "Nil", "Available")
+#"There are 3 books in this library."
