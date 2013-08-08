624e63d58d922ee58cac038690083af525199cac
Tina Kumar <tinakumar07@gmail.com>
2013-08-08 10:07:03 -0700

    fixed unshelf method

--- public_library.rb
+++ public_library.rb
@@ -1,5 +1,4 @@
 class Book < ActiveRecord::Base
-  # attr_accessible :title, :body
   attr_accessible :title, :author, :available, :shelf_id
 
   belongs_to :shelf
@@ -7,35 +6,35 @@ class Book < ActiveRecord::Base
 
   def enshelf
     #gives book's present location
-    return self.shelf_id
+    location = self.shelf_id
   end
 
   def unshelf
     #maybe staging area, if book is not in proper location
     if self.title.start_with?("A", "B", "C", "D", "E", "F", "G")
-      return self.title.capitalize + "is located at staging shelf A3."
+      location == "ST3"
     else
-      return self.title.capitalize + "is located at staging shelf A9."
+      location == "ST9"
     end
   end
 end
 
 class Library < ActiveRecord::Base
-  # attr_accessible :title, :body
+
   has_many :shelves
   has_many :books
 
   def book_inventory
-    @@books = Book.all
+    @books = Book.all
   end
 
   def number_of_shelves
-    @@shelf = Shelf.all.count
+    @shelf = Shelf.all.count
   end
 end
 
 class Shelf < ActiveRecord::Base
-  # attr_accessible :title, :body
+
   has many :books
   belongs_to :library
 
